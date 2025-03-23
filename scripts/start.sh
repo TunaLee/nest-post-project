4df93599dfa3a308defdca83825a2f1fa27614e1
tunalee <dongwon1103@naver.com>
2025-03-24 06:28:58 +0900

    fix: start.sh 수정

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 APP_DIR="/home/ec2-user/build"
 APP_NAME="nestjs-app"
+PM2_PATH=$(which pm2)
 
 echo "Starting application: $APP_NAME"
 
 cd $APP_DIR
 
-pm2 stop $APP_NAME
-pm2 start dist/main.js --name $APP_NAME
+
+$PM2_PATH stop $APP_NAME
+$PM2_PATH start dist/main.js --name $APP_NAME
