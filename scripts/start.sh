c37f29b51041ae2e66a83bc13fdfef3d58cbaf0f
tunalee <dongwon1103@naver.com>
2025-03-25 01:30:11 +0900

    fix: start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -12,8 +12,6 @@ npm install -g pm2
 PM2_PATH=$(which pm2)
 $PM2_PATH stop $APP_NAME || echo "$APP_NAME 애플리케이션이 실행 중이지 않습니다."
 
-
 $PM2_PATH start dist/main.js --name $APP_NAME
-
 
 $PM2_PATH status
