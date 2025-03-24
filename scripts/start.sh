9135e3f907cd6fa720481506e4ec2dbd4f9c24ed
tunalee <dongwon1103@naver.com>
2025-03-25 04:17:52 +0900

    fix: start.sh 수정

--- scripts/start.sh
+++ scripts/start.sh
@@ -7,11 +7,11 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 
-npm install -g pm2
+# 빌드
+cd $APP_DIR
+npm install
+npm run build
 
-PM2_PATH=$(which pm2)
-$PM2_PATH stop $APP_NAME || echo "$APP_NAME 애플리케이션이 실행 중이지 않습니다."
-
-$PM2_PATH start dist/main.js --name $APP_NAME
-
-$PM2_PATH status
+# pm2 사용하여 애플리케이션 시작
+pm2 start dist/main.js --name $APP_NAME
+pm2 status
