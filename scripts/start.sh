d97883d19c1613782a053c92d6de902e4b76d9fb
tunalee <dongwon1103@naver.com>
2025-03-25 01:23:56 +0900

    fix: shell script 주석 삭제

--- scripts/start.sh
+++ scripts/start.sh
@@ -4,16 +4,16 @@ APP_DIR="/home/ec2-user/build"
 APP_NAME="nestjs-app"
 
 export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 
 npm install -g pm2
 
 PM2_PATH=$(which pm2)
 $PM2_PATH stop $APP_NAME || echo "$APP_NAME 애플리케이션이 실행 중이지 않습니다."
 
-# 애플리케이션 시작
+
 $PM2_PATH start dist/main.js --name $APP_NAME
 
-# pm2 상태 확인 (선택사항)
+
 $PM2_PATH status
